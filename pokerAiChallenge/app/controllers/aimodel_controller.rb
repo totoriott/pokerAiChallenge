8de70b35f8c8c93085cdc1706507e740d8f82bc7
Eric Aleshire <ealeshire@storm8.com>
2017-01-29 01:01:49 -0800

    move pokerEngine to app folder so it loads and works

--- pokerAiChallenge/app/controllers/aimodel_controller.rb
+++ pokerAiChallenge/app/controllers/aimodel_controller.rb
@@ -1,5 +1,8 @@
+require 'CasinoEngine'
+
 class AimodelController < ApplicationController
 	def index
+		casinoEngine = CasinoEngine.new
 		@testing = "go do the 5.2 The first form section later!!!"
 	end
 end
