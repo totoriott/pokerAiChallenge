45e3b8840c0a6c34302cc019e7307f48ed28e9c9
Eric Aleshire <ealeshire@storm8.com>
2017-01-28 19:09:39 -0800

    ai rule for keeping hand of value

--- pokerAiChallenge/app/models/pokerEngine/AiModel.rb
+++ pokerAiChallenge/app/models/pokerEngine/AiModel.rb
@@ -1,5 +1,8 @@
+require_relative 'CasinoEngine'
+
 class AiRule
 	RULE_TYPE_JOKER = 0;
+	RULE_TYPE_HAND_OF_VALUE = 1;
 
 	AI_ACTION_KEEP_AND_CONTINUE = 0;
 	AI_ACTION_KEEP_AND_STOP = 1;
@@ -28,14 +31,29 @@ class AiModel
 		@rules = myRules
 
 		# testing
+		# todo: joker won't mesh properly with other rules yet
+		@rules.push(AiRule.new(AiRule::RULE_TYPE_HAND_OF_VALUE, 1, AiRule::AI_ACTION_KEEP_AND_STOP))
 		@rules.push(AiRule.new(AiRule::RULE_TYPE_JOKER, 1, AiRule::AI_ACTION_KEEP_AND_STOP))
 	end
 
 	def cardsThatMatchRule(hand, rule)
 		case rule.ruleType
+		when AiRule::RULE_TYPE_HAND_OF_VALUE 
+			# TODO: pass in casino engine
+			casinoEngine = CasinoEngine.new
+			valueOfHand = casinoEngine.betMultiplierForHand(hand)
+			if valueOfHand >= rule.count
+				hand
+			else
+				[]
+			end
 		when AiRule::RULE_TYPE_JOKER
 			jokerCards = hand.select { |card| card.isJoker == true }
-			jokerCards
+			if jokerCards.length >= rule.count
+				jokerCards
+			else
+				[]
+			end
 		else
 			[]
 		end
@@ -47,7 +65,7 @@ class AiModel
 
 		@rules.each do |rule|
 			matchCards = cardsThatMatchRule(hand, rule)
-			if matchCards.length >= rule.count
+			if matchCards.length >= 0
 				if rule.action == AiRule::AI_ACTION_KEEP_AND_CONTINUE
 					# todo: implement
 					ok = 2
