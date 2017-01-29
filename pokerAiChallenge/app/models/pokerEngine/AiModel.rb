8ef8c87c7933fb40aaa9a5599a7211a573d5fdff
Eric Aleshire <ealeshire@storm8.com>
2017-01-28 19:29:02 -0800

    keep X of a kind rule

--- pokerAiChallenge/app/models/pokerEngine/AiModel.rb
+++ pokerAiChallenge/app/models/pokerEngine/AiModel.rb
@@ -5,6 +5,7 @@ class AiRule
 	RULE_TYPE_JOKER = 0;
 	RULE_TYPE_HAND_OF_VALUE = 1;
 	RULE_TYPE_SAME_SUIT = 2;
+	RULE_TYPE_SAME_VALUE = 3;
 
 	AI_ACTION_KEEP_AND_CONTINUE = 0;
 	AI_ACTION_KEEP_AND_STOP = 1;
@@ -35,12 +36,18 @@ class AiModel
 		# testing
 		# todo: joker won't mesh properly with other rules yet
 		@rules.push(AiRule.new(AiRule::RULE_TYPE_HAND_OF_VALUE, 1, AiRule::AI_ACTION_KEEP_AND_STOP))
-		#@rules.push(AiRule.new(AiRule::RULE_TYPE_SAME_SUIT, 4, AiRule::AI_ACTION_KEEP_AND_STOP))
+		@rules.push(AiRule.new(AiRule::RULE_TYPE_SAME_SUIT, 4, AiRule::AI_ACTION_KEEP_AND_STOP))
+		@rules.push(AiRule.new(AiRule::RULE_TYPE_SAME_VALUE, 2, AiRule::AI_ACTION_KEEP_AND_STOP))
 		@rules.push(AiRule.new(AiRule::RULE_TYPE_JOKER, 1, AiRule::AI_ACTION_KEEP_AND_STOP))
 	end
 
+	# todo: maybe these can be moved
 	def cardsInHandOfSuit(hand, suit)
 		hand.select { |card| card.suit == suit }
+	end
+
+	def cardsInHandOfValue(hand, value)
+		hand.select { |card| card.value == value }
 	end
 
 	def cardsThatMatchRule(hand, rule)
@@ -68,6 +75,16 @@ class AiModel
 				# todo: handle if you have multiple such cases
 				suit = groupedSuits[rule.count][0]
 				cardsInHandOfSuit(hand, suit)
+			else
+				[]
+			end
+		when AiRule::RULE_TYPE_SAME_VALUE
+			handEvaluator = PokerHandEvaluator.new
+			groupedValues = handEvaluator.cardsGroupedByValue(hand)
+			if !groupedValues[rule.count].nil?
+				# todo: handle if you have multiple such cases
+				value = groupedValues[rule.count][0]
+				cardsInHandOfValue(hand, value)
 			else
 				[]
 			end
